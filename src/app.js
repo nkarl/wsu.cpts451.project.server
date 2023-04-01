e68f7cd8fac3ac1f747b73846c297a9b89696aff
nkarl <21993921+nkarl@users.noreply.github.com>
2023-04-01 09:33:41 -0700

    Converted code from ES5 to ES6.

--- src/app.js
+++ src/app.js
@@ -1,21 +1,16 @@
-var logger = require("morgan");
-var express = require("express");
-var cookieParser = require("cookie-parser");
+import logger from 'morgan';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import indexRouter from './routes/index';
 
-var path = require("path");
+const app = express();
 
-var indexRouter = require("./routes/index");
-//var usersRouter = require("./routes/users");
-
-var app = express();
-
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/v1", indexRouter);
-//app.use("/users", usersRouter);
+app.use('/v1', indexRouter);
 
-module.exports = app;
+export default app;
